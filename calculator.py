338cf2aff7c54284b71e39d6491e8a5079bd6667
henzel-ops <henzelmoraswork@gmail.com>
2026-02-05 00:58:10 +0530

    deleted files

--- calculator.py
+++
@@ -1,7 +0,0 @@
-x = float(input("Enter first number: "))
-y = float(input("Enter second number: "))
-
-print("Addition:", round(x + y))
-print("Subtraction:", round(x - y))
-print("Multiplication:", round(x * y))
-print("Division:", x / y)
--- greeting.py
+++
@@ -1,4 +0,0 @@
-f_name = str(input("Enter your first name: "))
-l_name = str(input("Enter your last name: ")) 
-
-print(f"Hello, {f_name} {l_name}! Welcome to the Python program.")
