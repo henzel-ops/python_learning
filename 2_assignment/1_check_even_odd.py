b1792354ac909e5a319d87c7619b71bdcfe3862c
henzel-ops <henzelmoraswork@gmail.com>
2026-02-12 19:49:02 +0530

    completed assignment 2

---
+++ 2_assignment/1_check_even_odd.py
@@ -0,0 +1,2 @@
+num = int(input("Enter a number: "))
+print(f"{num} is even number") if num % 2 == 0 else print(f"{num} is odd number")
---
+++ 2_assignment/2_sum_of_integers.py
@@ -0,0 +1,4 @@
+result = 0
+for num in range(1, 51):
+    result += num
+print(f"The sum of integers from 1 to 50 is: {result}")
